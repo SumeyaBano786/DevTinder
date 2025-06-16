c0b73cb8b89e35a9bf763087107af719946d2460
Sumeya Bano <sumeyabano99@gmail.com>
2025-06-16 15:50:34 +0530

    middleware created

--- src/app.js
+++ src/app.js
@@ -3,17 +3,6 @@ const app=express();
 
 
 
-app.use('/hello',(req,res)=>{
-    res.send("hello hello hello");
-})
-
-app.use('/test',(req,res)=>{
-    res.send("test test test");
-})
-app.use('/',(req,res)=>{
-    res.send("hello devtinder");
-})
-
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 })
